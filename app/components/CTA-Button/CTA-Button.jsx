50f700f1e9113f0b988d2d9ba420b415a458bb9f
Myriam <siadmyriam@gmail.com>
2025-12-11 14:37:19 +0100

    Creation de arborescence du projet, des routes, des differentes pages

---
+++ app/components/CTA-Button/CTA-Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./CTA-Button.css"
+export default function CtaButton() {
+  return (
+    <>
+    <div>
+        <button className='' >
+
+        </button>
+    </div>
+    </>
+  );
+}
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,9 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx") ,
+    route("/connexion" , "routes/connexion.tsx"),
+    route("/inscription" , 'routes/inscription.tsx'),
+    route("/listecourse" , "routes/listecourse.tsx"),
+    route("/magasins", "routes/magasin.tsx" )
+] satisfies RouteConfig;
