ec94985faa05c09c9f7d0ccc4d0e05caec6864cc
Myriam <siadmyriam@gmail.com>
2025-12-11 15:58:51 +0100

    Ajout du paramtre type pour le component button

--- app/components/CTA-Button/CTA-Button.jsx
+++ app/components/CTA-Button/CTA-Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import "./CTA-Button.css"
-export default function CtaButton({label}) {
+export default function CtaButton({label , type}) {
   return (
     <>
     <div>
-        <button type="button" className="bg-orange-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{label}</button>
+        <button type={type} className="bg-orange-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{label}</button>
     </div>
     </>
   );
